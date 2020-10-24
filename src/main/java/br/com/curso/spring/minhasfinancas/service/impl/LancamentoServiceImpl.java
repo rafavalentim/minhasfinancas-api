7f4ecd11cc0e297a69b03bb38c234333a5f2fefe
rafael valentim <rafa.valentim@gmail.com>
2020-10-24 17:39:00 -0300

    Melhorias obter Saldo do Lancamento na tela Home

--- src/main/java/br/com/curso/spring/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/br/com/curso/spring/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -126,9 +126,9 @@ public class LancamentoServiceImpl implements LancamentoService{
 	@Transactional(readOnly = true)
 	public BigDecimal obterSaldoPorUsuario(Long id) {
 		
-		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA);
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.RECEITA, StatusLancamento.EFETIVADO);
 		
-		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.DESPESA, StatusLancamento.EFETIVADO);
 		
 		
 		if(receitas == null) {
